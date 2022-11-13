ffcb96ea35b51a6eaa0050d6c96eea207a172edc
Syed M. Abbas Haider Taqvi <79911677+smabbasht@users.noreply.github.com>
2022-11-14 01:02:24 +0500

    add 4 problems

---
+++ longestCommonPrefix.py
@@ -0,0 +1,13 @@
+def longestCommonPrefix(self, strs: List[str]) -> str:
+    result = []
+    strs.sort(key= lambda x: len(x))
+    for i in range(len(strs[0])):
+        for j in range(len(strs)):
+            if strs[j][i] !=strs[0][i]:
+                if len(result)==0:
+                    return ""
+                else:
+                    return "".join(result)
+        result.append(strs[0][i])
+    
+    return "".join(result)
---
+++ twoSum.py
@@ -0,0 +1,6 @@
+def twoSum(self, nums: list[int], target: int) -> list[int]:
+    for ind, num in enumerate(nums[:-1]):
+        for indx, numx in enumerate(nums[ind+1:]):
+            if num+numx == target:
+                return [ind, indx+1+ind]
+            
